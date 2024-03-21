df94a27f70fb1e3689acdc3bcc36fdba7dea234e
KHORSI Mustapha <aziz.rcb2011@gmail.com>
2024-03-21 10:59:32 +0100

    feat : project initialization.

---
+++ src/main/java/com/factorydigital/flixflex/FlixflexApplication.java
@@ -0,0 +1,13 @@
+package com.factorydigital.flixflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factorydigital/flixflex/FlixflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factorydigital.flixflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
