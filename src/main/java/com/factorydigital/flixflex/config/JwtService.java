70e45a5d27d1a533600815e3b632bb1e544bd11c
KHORSI Mustapha <aziz.rcb2011@gmail.com>
2024-03-23 22:38:18 +0100

    fix : fix using token parsing.

--- src/main/java/com/factorydigital/flixflex/config/JwtService.java
+++ src/main/java/com/factorydigital/flixflex/config/JwtService.java
@@ -58,7 +58,7 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJwt(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
     }
 
     private Key getSignInKey() {
