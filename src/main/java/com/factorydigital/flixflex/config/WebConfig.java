3b4b64b78ae893247c9a02c4f5c2b2d5d3a1894c
KHORSI Mustapha <aziz.rcb2011@gmail.com>
2024-05-25 23:58:43 +0100

    fix : allow CORS requests.

--- src/main/java/com/factorydigital/flixflex/config/WebConfig.java
+++ src/main/java/com/factorydigital/flixflex/config/WebConfig.java
@@ -17,10 +17,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://flixflex-backend-app-production.up.railway.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowCredentials(false);
+                        .allowCredentials(true);
             }
         };
     }
