3a1d73e772395602e2cde2d0d72c5b84aa850044
KHORSI Mustapha <aziz.rcb2011@gmail.com>
2024-05-25 23:35:27 +0100

    fix : allow CORS requests.

---
+++ src/main/java/com/factorydigital/flixflex/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.factorydigital.flixflex.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${ALLOWED_DOMAIN}")
+    private String allowedDomain;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("allowedDomain")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
